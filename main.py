e6c58dc042269798ca9309884c999198b531667f
Sunita Lanka <sunita.uscareer@gmail.com>
2018-04-12 18:23:23 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:launchcodedb2@localhost:8888/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(120))
+
+    def __init__(self, title):
+        self.title = title
+        blog.body = body 
+
+    def __repr__(self):
+        return '<Blog %r>' % self.title    
