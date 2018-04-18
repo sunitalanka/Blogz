aeb82995aa142e64eb8a449712f587add7a56d95
Sunita Lanka <sunita.uscareer@gmail.com>
2018-04-18 15:23:43 -0500

    initial commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:launchcodedb2@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Blogz:launchcodedb5@localhost:8889/Blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -14,11 +14,12 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self,title,body):
+    def __init__(self,title,body,owner):
         self.title = title
         self.body = body
- 
+        self.owner = owner
 
 
     def is_valid(self):
@@ -28,12 +29,62 @@ class Blog(db.Model):
         else:
             return False
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref= 'owner')
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
 
 
 @app.route("/")
 def index():
     
     return redirect("/blog")
+
+
+
+ @app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        username = User.query.filter_by(username=username).first()
+        if username and username.password == password:
+            session['username'] = username
+            flash("Logged in")
+            return redirect('/newpost')
+        else:
+            flash('User password incorrect, or user does not exist', 'error')
+
+            return render_template('login.html') 
+
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        # TODO - validate user's data
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/')
+        else:
+            # TODO - user better response messaging
+            return "<h1>Duplicate user</h1>"
+
+return render_template('signup.html')  
 
 
 
@@ -58,11 +109,12 @@ def blog_page():
     body_error = ""
     new_title = ""
     new_body = ""
+    owner = User.query.filter_by(username=session['username']).first()
 
     if request.method == "POST":
         new_title = request.form['title']
         new_body = request.form['body']
-        add_blog = Blog(new_title,new_body)
+        add_blog = Blog(new_title,new_body,owner)
 
         if  new_title == ""  or len(new_body) == 1 :
              title_error = "Please Specify the blog title"
